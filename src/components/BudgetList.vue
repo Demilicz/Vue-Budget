d839b7a1ef47c0ce19f8c3d8e68807a564e1ec90
Demilicz <Necrolick92@gmail.com>
2020-11-15 21:26:03 +0100

    first commit

---
+++ src/components/BudgetList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="budget-list-wrap">
+    <ElCard>
+      <span>{{header}}</span>
+    </ElCard>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BudgetList",
+  data: () => ({
+    header: "Budget List",
+  }),
+};
+</script>
+
+<style scoped>
+.budget-list-wrap {
+  max-width: 500px;
+  margin: auto;
+  font-size: 23px;
+}
+</style>
---
+++ src/components/TotalBalance.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="total-value green" v-if="total > 0">Balance: {{ total }}</div>
+  <div class="total-value red" v-else-if="total < 0">Balance: {{ total }}</div>
+  <div class="total-value" v-else>Balance: {{ total }}</div>
+</template>
+
+<script>
+export default {
+  name: "TotalBalance",
+  props: {
+    total: {
+      type: Number,
+      default: 0
+    }
+  }
+};
+</script>
+
+<style scoped>
+.total-value {
+  font-size: 26px;
+  text-transform: uppercase;
+  padding: 20px;
+  text-align: center;
+}
+.green {
+  color:green;
+}
+.red {
+  color: red;
+}
+</style>
